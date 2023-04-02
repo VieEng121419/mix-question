fc6e1a8990da88cdc4b7b9dc2d4a4bf0bb1cbc76
Anh Duong <anh.duong@apps-cyclone.com>
2023-04-02 23:39:20 +0700

    [feat] Handle export xlsx

--- src/sections/Step2/_step2.jsx
+++ src/sections/Step2/_step2.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import Button from "../../components/Button/Button";
 import Card from "../../components/Card/Card";
 import Section from "../../components/Section/Section";
--- src/utils/_helper.js
+++ src/utils/_helper.js
@@ -1,5 +1,6 @@
 import { formattedAnswer } from "./_convertAnswer";
 import { useQuestionsListParsed } from "./_customHook";
+import * as XLSX from "xlsx";
 
 export const handleOnChangeInputFile = (e) => {
     const fileReader = new FileReader();
@@ -18,16 +19,19 @@ const csvFileToArray = (string) => {
         //Remove 3 last charaters:',,,' for label
         item = item.slice(0, -3);
         const questionObj = {
-            id: index,
-            label: item,
+            question: {
+                id: index,
+                label: item,
+            },
         };
+        // if even number then add question into array else add answers into question
         if ((index + 1) % 2) {
             csvQuestionArrayParsed = [...csvQuestionArrayParsed, questionObj];
         } else {
-            csvQuestionArrayParsed.forEach((question) => {
-                if (question.id === index - 1) {
-                    question.answers = formattedAnswer(
-                        question.id,
+            csvQuestionArrayParsed.forEach((questionObj) => {
+                if (questionObj.question.id === index - 1) {
+                    questionObj.question.answers = formattedAnswer(
+                        questionObj.question.id,
                         item.split(",")
                     );
                 }
@@ -39,14 +43,14 @@ const csvFileToArray = (string) => {
 
 export const handleChangeAnswer = (id, value, callback = () => null) => {
     let questionListEdited = useQuestionsListParsed();
-    questionListEdited = questionListEdited.map((question) => {
-        let answerEdited = question.answers.map((answer) => {
+    questionListEdited = questionListEdited.map((questionObj) => {
+        let answerEdited = questionObj.question.answers.map((answer) => {
             if (answer.id === id) {
                 return { ...answer, label: value };
             }
             return answer;
         });
-        return { ...question, answers: answerEdited };
+        return { question: { ...questionObj.question, answers: answerEdited } };
     });
     callback(questionListEdited);
 };
@@ -75,8 +79,41 @@ export const handleShuffle = (questionsList, callback = () => null) => {
         const j = Math.floor(Math.random() * (i + 1));
         [shuffledArr[i], shuffledArr[j]] = [shuffledArr[j], shuffledArr[i]];
         //Shuffle answers from shuffled question list
-        const shuffledAnswersArr = shuffleAnswersList(shuffledArr[i].answers);
-        shuffledArr[i].answers = shuffledAnswersArr;
+        const shuffledAnswersArr = shuffleAnswersList(shuffledArr[i].question.answers);
+        shuffledArr[i].question.answers = shuffledAnswersArr;
     }
     callback(shuffledArr);
+    exportToExcel(shuffledArr);
+};
+
+export const exportToExcel = (data, fileName) => {
+    // Create a new workbook
+    const workbook = XLSX.utils.book_new();
+
+    // Create a new sheet and add data into sheet created
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    data.forEach((item, index) => {
+        const rowQ = index * 2 + 1;
+        let rowA = rowQ + 1;
+        XLSX.utils.sheet_add_aoa(
+            worksheet,
+            [[`${index + 1}. ${item.question.label}`]],
+            {
+                origin: `A${rowQ}`,
+            }
+        );
+
+        item.question.answers.map((ans, index) => {
+            XLSX.utils.sheet_add_aoa(
+                worksheet,
+                [[`${String.fromCharCode(index + 65)}. ${ans.label}`]],
+                {
+                    origin: `${String.fromCharCode(index + 65)}${rowA}`, //Each answer is each column by char code (ex: 65 -> A,...)
+                }
+            );
+        });
+    });
+    XLSX.utils.book_append_sheet(workbook, worksheet, "De thi moi");
+    // Export file Excel
+    XLSX.writeFile(workbook, "de_thi_moi.xlsx");
 };
